8842c04beeb073f5c58fc282b79f6f944ed23190
Chitresh Goyal <chitreshstar1@gmail.com>
2013-03-10 15:40:46 +0530

    login change

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
    
   
    acts_as_authentic do |c|
-     c.login_field = 'email'
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,10 +34,10 @@ ActiveRecord::Schema.define(:version => 20130103070343) do
   end
 
   create_table "user_sessions", :force => true do |t|
-    t.string   "session_id", :default => "", :null => false
+    t.string   "session_id", :null => false
     t.text     "data"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "user_sessions", ["session_id"], :name => "index_user_sessions_on_session_id"
@@ -45,13 +45,13 @@ ActiveRecord::Schema.define(:version => 20130103070343) do
 
   create_table "users", :force => true do |t|
     t.string   "name",                :default => "", :null => false
-    t.string   "login",               :default => "", :null => false
-    t.string   "crypted_password",    :default => "", :null => false
-    t.string   "password_salt",       :default => "", :null => false
-    t.string   "email",               :default => "", :null => false
-    t.string   "persistence_token",   :default => "", :null => false
-    t.string   "single_access_token", :default => "", :null => false
-    t.string   "perishable_token",    :default => "", :null => false
+    t.string   "login",                               :null => false
+    t.string   "crypted_password",                    :null => false
+    t.string   "password_salt",                       :null => false
+    t.string   "email",                               :null => false
+    t.string   "persistence_token",                   :null => false
+    t.string   "single_access_token",                 :null => false
+    t.string   "perishable_token",                    :null => false
     t.integer  "login_count",         :default => 0,  :null => false
     t.integer  "failed_login_count",  :default => 0,  :null => false
     t.datetime "last_request_at"
