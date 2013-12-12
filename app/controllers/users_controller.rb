9c582ecb1c2aa009ba92e972ac5dbff3ae3399a5
Chitresh Goyal <chitreshstar1@gmail.com>
2013-12-12 14:32:58 +0530

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,12 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    
+    respond_to do |format|
+	   format.html
+      format.json { render json: @user }
+    end
+    
   end
 
   def edit
